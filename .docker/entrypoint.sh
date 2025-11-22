63cf8ce414affda27cb8350835e009201527bd9d
Jasur Xursanov <xursanovofficial@gmail.com>
2025-11-22 17:08:14 +0500

    Update file entrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,6 +3,22 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# SQLite fayl manzili
+SQLITE_DB="/var/www/database/database.sqlite"
+
+echo "Checking SQLite database file..."
+
+# Agar fayl mavjud bo'lmasa yaratamiz
+if [ ! -f "$SQLITE_DB" ]; then
+    echo "database.sqlite not found, creating..."
+    # katalog mavjud ekanini ham tekshirib yaratib qo'yamiz
+    mkdir -p /var/www/database
+    touch "$SQLITE_DB"
+    chmod 664 "$SQLITE_DB"
+else
+    echo "database.sqlite already exists. Skipping..."
+fi
+
 echo "Running pre-start commands..."
 php artisan storage:link
 php artisan app:make-webhook
